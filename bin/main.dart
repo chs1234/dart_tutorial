54d97bd389e386ead14003192deec1862cb1459c
chs1234 <chs4413@gmail.com>
2020-04-15 13:48:23 +0900

    Arrow Syntax

--- bin/main.dart
+++ bin/main.dart
@@ -1,32 +1,15 @@
-// [Functions]
-
-// int add(int a, int b) {
-//   return a + b;
-// }
-
-//반환 타입을 지정하지 않아도 됨. but, 코드가독성 및 유지보수를 위해 선언해주는 것이 좋음.
-// add(int a, int b) {
-//   return a + b;
-// }
-
-//피보나치수열
-/**
- * n result
- * 1 1
- * 2 1
- * 3 2
- * 4 3
- */
-int fibonacci(int n) {
-  if (n == 0 || n == 1) return n;
-  return fibonacci(n - 1) + fibonacci(n - 2);
-}
+//[Arrow Syntax]
 
 void main() {
-  // int result = add(1, 2);
-  // print(result);
+  List ages = [10, 11, 21];
+  //var filteredAges = ages.where((age) => age > 10);
+  //print(filteredAges); // expected: (11, 21)
 
-  // 1,1,2,3,5,8
-  int result = fibonacci(4);
-  print(result);
+  ages.where((age) => age > 10).forEach(print);
+  // expected: 11, 21
+
+  var flyByObjects = ['yourturn', 'hello', 'abc'];
+  flyByObjects.where((name) => name.contains('turn')).forEach(print);
+  // expected: 'yourturn'
+
 }
