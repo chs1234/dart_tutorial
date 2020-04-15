6594077e483a7a518ea4e0139101cd3ba032153d
chs1234 <chs4413@gmail.com>
2020-04-15 14:11:54 +0900

    Class

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,37 @@
-//[import]
+// [Class]
 
-import 'calculator.dart';
+class Person {
+  String name;
+  int age;
+
+  // default
+  // Person(String name, int age) {
+  //   this.name = name;
+  //   this.age = age;
+  // }
+
+  Person(this.name, this.age);
+
+  // default
+  // Person.age(int age) {
+  //   this.age = age;
+  //   //this.name = "손님";
+  // }
+
+  Person.age(int age) : this("손님", age);
+
+  void study() {
+    print('$name($age) is studying code');
+  }
+}
 
 void main() {
-  int result = add(1, 2);
-  int result2 = multiply(2, 5);
+  Person person = Person("철수", 20); //인스턴스(객체) 생성
+  Person person2 = Person("영희", 24); //인스턴스(객체) 생성
 
-  print('더하기 결과값: $result, 곱하기 결과값: $result2');
+  person.study();  // 철수(20) is studying code
+  person2.study(); // 영희(24) is studying code
+
+  Person person3 = Person.age(23);
+  person3.study(); // null(23) is studying code
 }
