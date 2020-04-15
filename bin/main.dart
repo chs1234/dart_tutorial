cf41f5f0558ce6ebfef9d2903d93814a8b33ca08
chs1234 <chs4413@gmail.com>
2020-04-15 15:21:32 +0900

    async 2

--- bin/main.dart
+++ bin/main.dart
@@ -1,28 +1,71 @@
+class Spacecraft {
+  String name;
+  DateTime launchDate;
+
+  // Constructor, with syntactic sugar for assignment to members.
+  Spacecraft(this.name, this.launchDate) {
+    // Initialization code goes here.
+  }
+
+  // Named constructor that forwards to the default one.
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  int get launchYear =>
+      launchDate?.year; // read-only non-final property
+
+  // Method.
+  void describe() {
+    print('Spacecraft: $name');
+    if (launchDate != null) {
+      int years =
+          DateTime.now().difference(launchDate).inDays ~/
+              365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
+
 const oneSecond = Duration(seconds: 1);
 // ···
-// Future<void> printWithDelay(String message) async {
-//   print("기다리는 중....");
-//   await Future.delayed(oneSecond);
-//   print(message);
-// }
-
-// void main() {
-//   printWithDelay("출력");
-// }
-
-Future<int> printWithDelay(String message) async {
-  print("기다리는 중....");
+Future<void> printWithDelay(String message) async {
   await Future.delayed(oneSecond);
   print(message);
+}
 
-  //1을 리턴해보도록 유도
-  return 1;
+Stream<String> report(Spacecraft craft, Iterable<String> objects) async* {
+  for (var object in objects) {
+    await Future.delayed(oneSecond);
+    yield '${craft.name} flies by $object';
+  }
 }
 
 void main() async {
-  // int result = printWithDelay("출력!!"); //오류 발생
+  Spacecraft spacecraft = Spacecraft('spaceCraft', DateTime.now());
+  Stream<String> result = report(spacecraft, ['abc', 'def', 'ggg']);
+  
+  /**
+   * 방법 1.
+   * spaceCraft flies by abc
+   * spaceCraft flies by def
+   * spaceCraft flies by ggg
+   * 
+   */
+  // await for(String s in result) {
+  //   print(s);
+  // }
 
-  // async, await을 지정해주어야함.
-  int result = await printWithDelay("출력!!");
-  print(result);
+  /** 
+   * 방법 2.(비동기)
+   * "코드종료"
+   * spaceCraft flies by abc
+   * spaceCraft flies by def
+   * spaceCraft flies by ggg
+  */ 
+  // result.listen((s) {
+  //   print(s);
+  // });
+
+  print("코드종료");
 }
