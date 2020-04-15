0bf7caf19a7e48f222f8158809f3b055fd381f79
chs1234 <chs4413@gmail.com>
2020-04-15 13:43:21 +0900

    Functions

--- bin/main.dart
+++ bin/main.dart
@@ -1,19 +1,32 @@
+// [Functions]
+
+// int add(int a, int b) {
+//   return a + b;
+// }
+
+//반환 타입을 지정하지 않아도 됨. but, 코드가독성 및 유지보수를 위해 선언해주는 것이 좋음.
+// add(int a, int b) {
+//   return a + b;
+// }
+
+//피보나치수열
+/**
+ * n result
+ * 1 1
+ * 2 1
+ * 3 2
+ * 4 3
+ */
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 void main() {
-  // [Control flow Statements]
+  // int result = add(1, 2);
+  // print(result);
 
-  // default
-  // for (int month = 1; month <= 12; month++) {
-  //   print(month);
-  // }
-
-  List ages = [10, 11, 12];
-  for (var age in ages) {
-    print(age);
-  }
-
-  int year = 2010;
-  while (year < 2016) {
-    year += 1;
-    print(year);
-  }
+  // 1,1,2,3,5,8
+  int result = fibonacci(4);
+  print(result);
 }
