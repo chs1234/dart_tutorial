e4a71782969f27af1a6ae653d13a68ccb6525133
chs1234 <chs4413@gmail.com>
2020-04-15 14:24:00 +0900

    class 2

--- bin/main.dart
+++ bin/main.dart
@@ -1,37 +1,41 @@
-// [Class]
-
-class Person {
+class Spacecraft {
   String name;
-  int age;
+  DateTime launchDate;
 
-  // default
-  // Person(String name, int age) {
-  //   this.name = name;
-  //   this.age = age;
-  // }
+  // Constructor, with syntactic sugar for assignment to members.
+  Spacecraft(this.name, this.launchDate) {
+    // Initialization code goes here.
+    name = name + "님";
+  }
 
-  Person(this.name, this.age);
+  // Named constructor that forwards to the default one.
+  Spacecraft.unlaunched(String name) : this(name, null);
 
-  // default
-  // Person.age(int age) {
-  //   this.age = age;
-  //   //this.name = "손님";
-  // }
+  int get launchYear =>
+      launchDate?.year; // read-only non-final property
+      // ?. => null이 아닌 경우에만 반환함. null인 경우 반환하지 않는다.
 
-  Person.age(int age) : this("손님", age);
-
-  void study() {
-    print('$name($age) is studying code');
+  // Method.
+  void describe() {
+    print('Spacecraft: $name');
+    if (launchDate != null) {
+      int years =
+          DateTime.now().difference(launchDate).inDays ~/
+              365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
   }
 }
 
 void main() {
-  Person person = Person("철수", 20); //인스턴스(객체) 생성
-  Person person2 = Person("영희", 24); //인스턴스(객체) 생성
+  Spacecraft spacecraft = Spacecraft("나로호", DateTime.now());
+  //print(spacecraft); //Instance of 'Spacecraft'
+  //print("이름: ${spacecraft.name}, 발사일: ${spacecraft.launchDate}");
+  
+  //spacecraft.launchYear = 30; // 값 설정 불가, int "get"
+  print(spacecraft.launchYear);
 
-  person.study();  // 철수(20) is studying code
-  person2.study(); // 영희(24) is studying code
-
-  Person person3 = Person.age(23);
-  person3.study(); // null(23) is studying code
+  spacecraft.describe();
 }
