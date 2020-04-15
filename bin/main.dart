0913ff20d3d827d76dd7c797981ab52d8c286641
chs1234 <chs4413@gmail.com>
2020-04-15 15:07:59 +0900

    Async

--- bin/main.dart
+++ bin/main.dart
@@ -1,120 +1,28 @@
-// [Mixins]
+const oneSecond = Duration(seconds: 1);
+// ···
+// Future<void> printWithDelay(String message) async {
+//   print("기다리는 중....");
+//   await Future.delayed(oneSecond);
+//   print(message);
+// }
 
-class Spacecraft {
-  String name;
-  DateTime launchDate;
+// void main() {
+//   printWithDelay("출력");
+// }
 
-  // Constructor, with syntactic sugar for assignment to members.
-  Spacecraft(this.name, this.launchDate) {
-    // Initialization code goes here.
-  }
+Future<int> printWithDelay(String message) async {
+  print("기다리는 중....");
+  await Future.delayed(oneSecond);
+  print(message);
 
-  // Named constructor that forwards to the default one.
-  Spacecraft.unlaunched(String name) : this(name, null);
-
-  int get launchYear =>
-      launchDate?.year; // read-only non-final property
-
-  // Method.
-  void describe() {
-    print('Spacecraft: $name');
-    if (launchDate != null) {
-      int years =
-          DateTime.now().difference(launchDate).inDays ~/
-              365;
-      print('Launched: $launchYear ($years years ago)');
-    } else {
-      print('Unlaunched');
-    }
-  }
+  //1을 리턴해보도록 유도
+  return 1;
 }
 
-class Orbiter extends Spacecraft {
-  num altitude; //num은 int, double의 부모클래스, 모두 수용가능
-  Orbiter(String name, DateTime launchDate, this.altitude)
-      : super(name, launchDate); //부모의 생성자 호출
+void main() async {
+  // int result = printWithDelay("출력!!"); //오류 발생
 
-  @override
-  void describe() {
-    print("자식의 정보!!!");
-    super.describe();
-    print("부모의 describe()를 재정의함! altitude: $altitude");
-  }
-}
-
-class Piloted {
-  int astronauts = 1;
-  // **Mixins로 정의된 클래스는 생성자를 만들 수 없다.
-  //Piloted() {}
-  void describeCrew() {
-    print('Number of astronauts: $astronauts');
-  }
-}
-
-class PilotedCraft extends Spacecraft with Piloted {
-  PilotedCraft(String name, DateTime launchDate) : super(name, launchDate);
-}
-
-class MockSpaceship implements Spacecraft {
-  @override
-  DateTime launchDate;
-
-  @override
-  String name;
-
-  // 생성자 추가
-  MockSpaceship(this.launchDate, this.name);
-
-  @override
-  void describe() {
-    // TODO: implement describe
-  }
-
-  @override
-  // TODO: implement launchYear
-  int get launchYear => null;
-
-}
-
-abstract class Describable {
-  void describe();
-
-  void describeWithEmphasis() {
-    print('=========');
-    describe();
-    print('=========');
-  }
-}
-
-class Unit extends Describable {
-  @override
-  void describe() {
-    print('unit Class override');
-  }
-}
-
-class Animal implements Describable {
-  @override
-  void describe() {
-    print("describe()");
-  }
-
-  @override
-  void describeWithEmphasis() {
-    print("describeWithEmphasis()");
-  }
-
-}
-
-void main() {
-    // MockSpaceship mockSpaceship = MockSpaceship(DateTime.now(), "나로호");
-    // print(mockSpaceship.name); //null
-
-    // Unit unit = Unit();
-    // unit.describe();
-    // unit.describeWithEmphasis();
-
-    Animal animal = Animal();
-    animal.describe();
-    animal.describeWithEmphasis();
+  // async, await을 지정해주어야함.
+  int result = await printWithDelay("출력!!");
+  print(result);
 }
